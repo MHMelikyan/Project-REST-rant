d46943e084d0ebee0121a5f9d0967fc93fcb189a
Melik Melikyan <110069520+MHMelikyan@users.noreply.github.com>
2022-12-11 23:10:43 -0500

    added a edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -68,8 +68,31 @@ router.post('/', (req, res)=>{
    else {
     res.render('places/edit',{place: places[id]})
    }
+  })
 
-
+  //PUT:
+  router.put('/:id',(req,res) =>{
+    let id = Number(req.params.id)
+    if(isNaN(id)){
+        res.render('error404')
+    }
+    else if (!places[id]){
+        res.render('error404')
+    }
+    else{
+        if (!req.body.pic){
+        req.body.pic = 'http:placekitten.com/400/400'
+        }
+        if (!req.body.city){
+        req.body.city = 'Anytown'
+        }
+        if(!req.body.state){
+            req.body.state = 'USA'
+        }
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+        
+    }
   })
 
 module.exports = router
