b7c96b2c8b4d411199aa10664778e66f29887cdd
Melik Melikyan <110069520+MHMelikyan@users.noreply.github.com>
2022-12-16 01:43:50 -0500

    Rest-Rant H-Thai-ML

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,24 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data){
+    let comments=(
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if(data.place.comments.map (c=>{
+        return(
+            <div className="border">
+                 <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+            </div>
+        )
+    })
+    )
     return (
         <Def>
         <main>
@@ -39,6 +57,7 @@ function show(data){
             </form>
             <div className = "col-sm-6">
                 <h2> Comments</h2>
+                {comments}
                         <a href ={`/places/${data.place.id}/rant`} className='btn btn-danger'> Add a Rant</a>
             </div>
         </main>    
